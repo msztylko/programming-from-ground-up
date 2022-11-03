b677ed9c527bae4fd4f72005ed48399756ca9a35
Marcin Sztylko <marcin.sztylko@gmail.com>
2022-11-03 20:55:59 +0100

    add next program

--- first-programs/exit.s
+++ first-programs/exit.s
@@ -1,24 +1,7 @@
-# PURPOSE:  Simple program that exits and returns a
-#           status code back to the Linux kernel
-#
-
-# INPUT:    none
-#
-
-# OUTPUT:   returns a status code. This can ve viewed
-#           by typing
-#
-#           echo $?
-#
-
-# VARIABLES:
-#           %eax holds the system call number
-#           %ebx holds the return status
 .section .data
 
 .section .text
-.globl _start
 _start:
- mov1 $1, %eax
- mov1 $0, %ebx
+ movl $1, %eax
+ movl $0, %ebx
  int $0x80
---
+++ first-programs/maximum.s
@@ -0,0 +1,41 @@
+# PURPOSE:      This program finds the maximum number of a
+#               set of data items.
+#
+# VARIABLES:    The registers have the following uses:
+#
+# %edi - Holds the index of the data item being examined
+# %ebx - Largest data item found
+# %eax - Current data item
+#
+# The following memory locations are used:
+# data_items - contains the item data. A 0 is used 
+#              to terinate the dat 
+#
+
+.section .data
+
+data_items:
+ .long 3, 12, 54, 23, 95, 115, 234, 51, 20, 0
+
+ .section .text
+
+ .globl _start
+_start:
+ movl $0, %edi
+ movl data_items(,%edi,4), %eax
+ movl %eax, %ebx
+
+start_loop:
+ cmpl $0, %eax
+ je loop_exit
+ incl %edi
+ movl data_items(,%edi,4), %eax
+ cmpl %ebx, %eax
+ jle start_loop
+
+ movl %eax, %ebx
+ jmp start_loop
+
+loop_exit:
+ movl $1, %eax
+ int $0x80
