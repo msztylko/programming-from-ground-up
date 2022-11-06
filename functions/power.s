ce08956e2ef8f268dc6e32e3e9c0064a2435e1a7
Marcin Sztylko <marcin.sztylko@gmail.com>
2022-11-06 11:41:55 +0100

    cleanup

--- functions/power.s
+++ functions/power.s
@@ -45,24 +45,24 @@ _start:
 #           %eax is used for temporary storage
 .type power, @function
 power:
- pushl %ebp         # save old base point
- movl %esp, %ebp    # make stack pointer the base pointer
- subl $4, %esp      # get room for our local storage
+ pushl %ebp             # save old base point
+ movl %esp, %ebp        # make stack pointer the base pointer
+ subl $4, %esp          # get room for our local storage
 
- movl 8(%ebp), %ebx # put first argument in %eax
- movl 12(%ebp), %ecx # put second argument in %ecx
+ movl 8(%ebp), %ebx     # put first argument in %eax
+ movl 12(%ebp), %ecx    # put second argument in %ecx
 
- movl %ebx, -4(%ebp) # store current result
+ movl %ebx, -4(%ebp)    # store current result
 
 power_loop_start:
- cmpl $1, %ecx      # if the power is 1, finish
+ cmpl $1, %ecx          # if the power is 1, finish
  je end_power
- movl -4(%ebp), %eax # move the current result into %eax
- imull %ebx, %eax   # multiply the current result by 
-                    # the base number
- movl %eax, -4(%ebp) # store the current result
+ movl -4(%ebp), %eax    # move the current result into %eax
+ imull %ebx, %eax       # multiply the current result by 
+                        # the base number
+ movl %eax, -4(%ebp)    # store the current result
 
- decl %ecx          # decrease the power
+ decl %ecx              # decrease the power
  jmp power_loop_start
 
 end_power:
