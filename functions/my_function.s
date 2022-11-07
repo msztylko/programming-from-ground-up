6e65f22e5fcdbd4f0547e5ef3800e65844c4b1d5
Marcin Sztylko <marcin.sztylko@gmail.com>
2022-11-07 09:33:22 +0100

    more details

--- functions/my_function.s
+++ functions/my_function.s
@@ -3,14 +3,20 @@
 
 .globl _start
 _start:
-# 1. Push parameters in revers order
+# 1. Push parameters in reverse order
 pushl $3
 pushl $6
 # 2. call your function
+# this call is responsible for 2 things:
+#   - push return address onto the stack
+#   - move %eip pointer to point to function code
 call power
 # 8. get return value, cleanup parameters
+ # get return value from %eax
  movl %eax, %ebx
+ # get rid of parameters by reseting the stack pointer 
  addl $8, %esp
+ # exit sys call
  movl $1, %eax
  int $0x80
 
@@ -23,15 +29,16 @@ call power
 
 .type power, @function
 power:
-# 3. save %ebp on the stack and point %esp to it
+# 3. save %ebp on the stack and copy the stack pointer to %ebp
 pushl %ebp
 movl %esp, %ebp
 # 4. reserve space for local variables
+#    only on use in this function, so reserve 4 space
 subl $4, %esp
 # 5. prepare variables
-movl 8(%ebp), %eax
-movl 12(%ebp), %ecx
-movl %eax, -4(%ebp)
+movl 8(%ebp), %eax  # load base
+movl 12(%ebp), %ecx # load exponent
+movl %eax, -4(%ebp) # set current result to base
 # 6. processing
 power_loop:
  cmpl $1, %ecx
@@ -44,7 +51,10 @@ power_loop:
 
 # 7. exit
 exit:
+ # move result to %eax
  movl -4(%ebp), %eax
+ # reset stack pointer
  movl %ebp, %esp 
  pop %ebp
+ # return and set instruction pointer back to caller 
  ret
